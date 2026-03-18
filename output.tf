8345764e450962b807b4af2450452dc12db34fe8
tarkanic909 <tarkanic909@gmail.com>
2026-03-18 15:47:02 +0100

    style: fix terraform fmt on output.tf

--- output.tf
+++ output.tf
@@ -2,14 +2,14 @@ output "node_info" {
   value = {
     for node_name in keys(var.nodes) :
     node_name => {
-      name         = node_name
-      mgmt_ip      = local.mgmt_ips[node_name]
-      lan_ip       = var.nodes[node_name].lan_ip
-      interlink_ip = lookup(local.interlink_ips, node_name, "")
-      memory       = var.nodes[node_name].memory
-      vcpu         = var.nodes[node_name].vcpu
-      role         = var.nodes[node_name].role
-      as           = var.nodes[node_name].as
+      name            = node_name
+      mgmt_ip         = local.mgmt_ips[node_name]
+      lan_ip          = var.nodes[node_name].lan_ip
+      interlink_ip    = lookup(local.interlink_ips, node_name, "")
+      memory          = var.nodes[node_name].memory
+      vcpu            = var.nodes[node_name].vcpu
+      role            = var.nodes[node_name].role
+      as              = var.nodes[node_name].as
       bgp_neighbor_ip = lookup(local.interlink_ips, lookup(local.interlink_neighbors, node_name, ""), "")
     }
   }
